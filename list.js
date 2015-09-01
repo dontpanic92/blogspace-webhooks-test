ee626e63d70cda3f9bb2814964ee6a611cdde79e
Shengqiu Li <tuesdayslineara@gmail.com>
2015-09-02 01:28:10 +0800

    Site updated: 2015-09-02 01:28:10

--- list.js
+++ list.js
@@ -1,3 +1,3 @@
-posts = [{title:'working-normally-now', date:'2015-09-02', url:'http://dontpanic.gitcafe.io/runhit/2015/09/02/working-normally-now/'}
+posts = [{title:'镜像源服务器流出数据已恢复正常', date:'2015-09-02', url:'http://dontpanic.gitcafe.io/runhit/2015/09/02/working-normally-now/'}
 ,{title:'敏感词过滤导致下载文件损坏', date:'2015-08-23', url:'http://dontpanic.gitcafe.io/runhit/2015/08/23/keyword-filtered-problem/'}
 ];
