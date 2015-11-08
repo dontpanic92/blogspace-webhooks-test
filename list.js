323328d0cfa10db7e688da8218acd03be7ddc516
Shengqiu Li <tuesdayslineara@gmail.com>
2015-11-08 11:44:11 +0800

    Site updated: 2015-11-08 11:44:11

--- list.js
+++ list.js
@@ -1,5 +1,5 @@
-posts = [{title:'磁盘阵列已修复完毕', date:'2015-10-22', url:'http://dontpanic.gitcafe.io/runhit/2015/10/22/diskarray-is-back/'}
-,{title:'磁盘阵列损坏', date:'2015-10-19', url:'http://dontpanic.gitcafe.io/runhit/2015/10/19/diskarray-is-missing/'}
-,{title:'镜像源服务器流出数据已恢复正常', date:'2015-09-02', url:'http://dontpanic.gitcafe.io/runhit/2015/09/02/working-normally-now/'}
-,{title:'敏感词过滤导致下载文件损坏', date:'2015-08-23', url:'http://dontpanic.gitcafe.io/runhit/2015/08/23/keyword-filtered-problem/'}
+posts = [{title:'磁盘阵列已修复完毕', date:'2015-10-22', url:'http://dontpanic92.github.io/blogspace-webhooks-test/2015/10/22/diskarray-is-back/'}
+,{title:'磁盘阵列损坏', date:'2015-10-19', url:'http://dontpanic92.github.io/blogspace-webhooks-test/2015/10/19/diskarray-is-missing/'}
+,{title:'镜像源服务器流出数据已恢复正常', date:'2015-09-02', url:'http://dontpanic92.github.io/blogspace-webhooks-test/2015/09/02/working-normally-now/'}
+,{title:'敏感词过滤导致下载文件损坏', date:'2015-08-23', url:'http://dontpanic92.github.io/blogspace-webhooks-test/2015/08/23/keyword-filtered-problem/'}
 ];
