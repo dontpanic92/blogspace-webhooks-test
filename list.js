3bde4e489f445de1a074a74bc0b7d0d7dafd86de
Shengqiu Li <tuesdayslineara@gmail.com>
2015-11-08 14:26:40 +0800

    Site updated: 2015-11-08 14:26:40

--- list.js
+++ list.js
@@ -1,6 +1,6 @@
-posts = [{title:'newpost', date:'2015-11-08', url:'http://dontpanic92.github.io/blogspace-webhooks-test/2015/11/08/newpost/'}
+posts = [{title:'anothernewpost', date:'2015-11-08', url:'http://dontpanic92.github.io/blogspace-webhooks-test/2015/11/08/anothernewpost/'}
+,{title:'newpost', date:'2015-11-08', url:'http://dontpanic92.github.io/blogspace-webhooks-test/2015/11/08/newpost/'}
 ,{title:'hello world', date:'2015-11-08', url:'http://dontpanic92.github.io/blogspace-webhooks-test/2015/11/08/hello-world/'}
-,{title:'磁盘阵列已修复完毕', date:'2015-10-22', url:'http://dontpanic92.github.io/blogspace-webhooks-test/2015/10/22/diskarray-is-back/'}
 ,{title:'镜像源服务器流出数据已恢复正常', date:'2015-09-02', url:'http://dontpanic92.github.io/blogspace-webhooks-test/2015/09/02/working-normally-now/'}
 ,{title:'敏感词过滤导致下载文件损坏', date:'2015-08-23', url:'http://dontpanic92.github.io/blogspace-webhooks-test/2015/08/23/keyword-filtered-problem/'}
 ];
